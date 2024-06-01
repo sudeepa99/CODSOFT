e89101c28170c75938020547354f82e88f982dad
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-01 10:52:02 +0530

    Taking user inputs for number of subjects

---
+++ Task2/GradeCalculator.java/src/GradeCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) throws Exception {
+        // Taking user inputs for number of subjects
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Enter the number of subjects:");
+        int no_sub= scanner.nextInt();
+
+    }
+}
