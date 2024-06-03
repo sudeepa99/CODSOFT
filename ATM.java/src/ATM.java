fa16b485a3928061b806df72ab3ff4835cbb02a2
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-03 23:38:16 +0530

    Created methods for withdrwaing,deposit and checking balance

--- ATM.java/src/ATM.java
+++ ATM.java/src/ATM.java
@@ -1,5 +1,22 @@
-public class App {
+public class ATM {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    }
+
+
+    public double WithdrawMoney(){
+
+
+        return 0;
+
+    }
+
+    public double DepositMoney(){
+        return 0;
+    }
+
+    public double CheckBalance(){
+        return 0;
+
     }
 }
