584a1fa7dd97232a7fddc9f023babb9da5144eda
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-04 22:47:43 +0530

    Getting user inputs for deposit amount

--- ATM.java/src/ATM.java
+++ ATM.java/src/ATM.java
@@ -1,22 +1,40 @@
+import java.util.Scanner;
+
 public class ATM {
     public static void main(String[] args) throws Exception {
+    
+        double with_money;
+
+        //taking user from the deposit money
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter the deposit money amount :");
+        double dep_money= scanner.nextDouble();
+        scanner.close();
+
+        
+        
+        
         
     }
 
 
-    public double WithdrawMoney(){
+    public void WithdrawMoney(){
 
 
-        return 0;
+    
 
     }
 
-    public double DepositMoney(){
-        return 0;
+    public void DepositMoney(){
+
+        
+
+
+        
     }
 
-    public double CheckBalance(){
-        return 0;
+    public void CheckBalance(){
+        
 
     }
 }
