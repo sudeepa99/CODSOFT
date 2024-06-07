e7d570db5d879fabd1d66a2044802b60fb4715cd
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-07 13:10:42 +0530

    Completed bank account class

--- ATM.java/src/BankAccount.java
+++ ATM.java/src/BankAccount.java
@@ -1,4 +1,30 @@
 public class BankAccount {
-    public double acc_balance;
+    private double acc_balance;
+
+    public BankAccount(double init_balance){
+        this.acc_balance=init_balance;
+    }
+
+    public double getBalance(){
+        return acc_balance;
+
+    }
+
+    public void deposit(double amount){
+        if (amount>0) {
+            acc_balance+=amount;
+            
+        }
+
+    }
+
+    public boolean withdraw(double amount){
+        if (amount>0 && acc_balance >= amount) {
+            acc_balance-=amount;
+            return true;
+            
+        }
+        return false;
+    }
     
 }
