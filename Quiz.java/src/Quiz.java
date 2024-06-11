962d1314bfa2093814a1517bad7135a87b2ede67
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-11 23:36:07 +0530

    Added 3 questions and their answers

--- Quiz.java/src/Quiz.java
+++ Quiz.java/src/Quiz.java
@@ -7,6 +7,9 @@ public class Quiz {
             {"What is the capital of France?","Berlin","Madrid","Paris","Rome"},
             {"What is the chemical symbol for water?","H2O","CO2","NO2","HCL"},
             {"Who wrote the play \"Romeo and Juliet\"?","William Shakespeare","Charles Dickens","J.K. Rowling","Mark Twain"},
+            {"What is the largest planet in our solar system?", "Earth", "Mars", "Jupiter", "Saturn"},
+            {"What is the square root of 64?", "6", "7", "8", "9"},
+            {"Which country is known as the Land of the Rising Sun?", "China", "Japan", "South Korea", "Thailand"},
 
 
         };
