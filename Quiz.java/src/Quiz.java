5d026a8ae6e9c737a37d09761fcc42957f446d9b
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-11 23:45:50 +0530

    completed adding questions and their answers

--- Quiz.java/src/Quiz.java
+++ Quiz.java/src/Quiz.java
@@ -10,6 +10,10 @@ public class Quiz {
             {"What is the largest planet in our solar system?", "Earth", "Mars", "Jupiter", "Saturn"},
             {"What is the square root of 64?", "6", "7", "8", "9"},
             {"Which country is known as the Land of the Rising Sun?", "China", "Japan", "South Korea", "Thailand"},
+            {"Who is the author of the Harry Potter series?", "J.K. Rowling", "J.R.R. Tolkien", "Stephen King", "George R.R. Martin"},
+            {"What is the smallest prime number?", "0", "1", "2", "3"},
+            {"What is the capital city of Australia?", "Sydney", "Melbourne", "Brisbane", "Canberra"},
+            {"Which planet is known as the Red Planet?", "Venus", "Mars", "Jupiter", "Mercury"}
 
 
         };
